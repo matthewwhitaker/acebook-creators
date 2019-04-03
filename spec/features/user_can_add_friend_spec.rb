29c22fd1c6dfe7c3afd9556048542cd6045c90f9
Lima Vadakara <lima.vadakara@gmail.com>
2019-04-03 23:01:36 +0100

    User can add a friend - when viewing a user that is a friend, message tells the user that they are friends. The Add Friend button does not show anymore. A user can add only another person as their friend

--- spec/features/user_can_add_friend_spec.rb
+++ spec/features/user_can_add_friend_spec.rb
@@ -8,7 +8,10 @@ RSpec.feature 'As a user', type: :feature, js: true do
     create_new_post('Hello')
     click_link another_user.id.to_s
     expect(current_path).to eq "/users/#{another_user.id}"
+    expect(page).to have_button 'Back to feed'
+    expect(page).not_to have_content 'You are friends with this user'
     click_button 'Add Friend'
-    expect(page).to have_button 'Un Friend'
+    expect(page).to have_content 'You are friends with this user'
+    expect(page).to have_button 'Back to feed'
   end
 end
