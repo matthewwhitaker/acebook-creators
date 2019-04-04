8a5f3701f5a680f766b439c060d864d83ddf9fd7
Lima Vadakara <lima.vadakara@gmail.com>
2019-04-04 17:26:39 +0100

    The user first and last name can be viewed against the post when viewing list of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.includes(:user).all.order('created_at DESC')
   end
 
   def update
--- spec/features/user_can_add_friend_spec.rb
+++ spec/features/user_can_add_friend_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature 'As a user', type: :feature, js: true do
   scenario 'I can add a friend' do
-    another_user = User.create(email: 'another@an.com', password: '123456')
+    another_user = User.create(firstname: 'Joe', lastname: 'Bloggs', email: 'another@an.com', password: '123456')
     Post.create(message: 'Hey!!!', user_id: another_user.id.to_s)
     user_sign_up
     create_new_post('Hello')
-    click_link another_user.id.to_s
+    click_link "#{another_user.firstname} #{another_user.lastname}"  
     expect(current_path).to eq "/users/#{another_user.id}"
     expect(page).to have_button 'Back to feed'
     expect(page).not_to have_content 'You are friends with this user'
